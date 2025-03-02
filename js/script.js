651d4795220a3718d8a0ce0c9f1e2e058fdf6ca5
AndryAlexis <andry.html@gmail.com>
2025-03-02 15:40:51 +0100

    Almost done

---
+++ js/script.js
@@ -0,0 +1,24 @@
+import { clamp } from './utils/helper.js';
+import { CSS_VARIABLES, MAX_DEGREES, ACCELERATION, SCROLL_EVENT, MAIN_TAG, DEGREES, PERCENTAGE } from './utils/constants.js';
+
+window.onload = () => {
+    let currentInvitationState = 0
+    let currentInvitationPosition = 0
+
+    const main = document.querySelector(MAIN_TAG);
+
+    main.addEventListener(SCROLL_EVENT, () => {
+        const normalizedScrollPosition = main.scrollTop / (main.scrollHeight - main.clientHeight);
+        currentInvitationState = clamp(MAX_DEGREES * normalizedScrollPosition * ACCELERATION, 0, MAX_DEGREES);
+        document.documentElement.style.setProperty(CSS_VARIABLES.INVITATION_STATE, `${currentInvitationState * -1}${DEGREES}`);
+
+
+        if (normalizedScrollPosition <= 0.5) {
+            currentInvitationPosition = clamp(normalizedScrollPosition * ACCELERATION, 0, 1);
+        } else {
+            currentInvitationPosition = clamp((1 - normalizedScrollPosition) * ACCELERATION, 0, 1);
+        }
+
+        document.documentElement.style.setProperty(CSS_VARIABLES.INVITATION_POSITION, `${currentInvitationPosition * 100}${PERCENTAGE}`);
+    });
+}
---
+++ js/utils/constants.js
@@ -0,0 +1,21 @@
+const CSS_VARIABLES = {
+    INVITATION_STATE: '--invitation-state',
+    INVITATION_POSITION: '--invitation-position',
+}
+
+const MAX_DEGREES = 180;
+const ACCELERATION = 2;
+const SCROLL_EVENT = 'scroll';
+const MAIN_TAG = 'main';
+const DEGREES = 'deg';
+const PERCENTAGE = '%';
+
+export {
+    CSS_VARIABLES,
+    MAX_DEGREES,
+    ACCELERATION,
+    SCROLL_EVENT,
+    MAIN_TAG,
+    DEGREES,
+    PERCENTAGE
+}
